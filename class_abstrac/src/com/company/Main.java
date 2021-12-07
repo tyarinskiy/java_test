c2521fec415cab9fccd15d409ebb7e5ea6db5c40
tyarinskiy <tyarin@list.ru>
2021-12-07 17:54:49 +0300

    обстрактный класс. рисуем фигуры

---
+++ class_abstrac/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Shape circle = new Circle();
+        circle.draw();
+    } //obstar class ne mozet sam vizivat metod. i ne mozet imet polei
+}
