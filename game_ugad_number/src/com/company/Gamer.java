142a0e4f1db4d98dafa7842af3dff4ccc7eff53a
tyarinskiy <tyarin@list.ru>
2021-12-13 00:05:31 +0300

    добавил объекты и методы в основной класс gamer

--- game_ugad_number/src/com/company/Gamer.java
+++ game_ugad_number/src/com/company/Gamer.java
@@ -1,14 +1,23 @@
 package com.company;
 
 public abstract class Gamer {
-
+    private String myName, animyName;
     private int number;
 
-//    abstract public void printResult ();
-
+    public String getMyName(){
+        return myName;
+    }
+    public String getAnimyName(){
+        return animyName;
+    }
+    public Gamer(String myName, String animyName){
+        this.myName = myName;
+        this.animyName = animyName;
+    }
     public int getNumber () {
         return number;
     }
+
 
 
 
